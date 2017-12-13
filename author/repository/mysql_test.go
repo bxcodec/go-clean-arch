0a133b2ea02b3384daf6a9407f4d43621fce7f52
Iman Tumorang <bxcodec@users.noreply.github.com>
2017-12-13 15:51:30 +0700

    :sparkles: add Author Domain

---
+++ author/repository/mysql_test.go
@@ -0,0 +1,32 @@
+package repository_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/bxcodec/go-clean-arch/author/repository"
+	"github.com/stretchr/testify/assert"
+	sqlmock "gopkg.in/DATA-DOG/go-sqlmock.v1"
+)
+
+func TestGetByID(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+	rows := sqlmock.NewRows([]string{"id", "name", "updated_at", "created_at"}).
+		AddRow(1, "Iman Tumorang", time.Now(), time.Now())
+
+	query := "SELECT id, name, created_at, updated_at WHERE id=\\?"
+
+	prep := mock.ExpectPrepare(query)
+	userID := int64(1)
+	prep.ExpectQuery().WithArgs(userID).WillReturnRows(rows)
+
+	a := repository.NewMysqlAuthorRepository(db)
+
+	anArticle, err := a.GetByID(userID)
+	assert.NoError(t, err)
+	assert.NotNil(t, anArticle)
+}
