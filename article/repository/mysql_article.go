eb3803f88d1a4bce0e871bdd004da60b5326fb0a
Iman Tumorang <bxcodec@gmail.com>
2018-11-25 14:33:55 +0700

    remove unnecessary comment by typo because of daydreaming :v

--- article/repository/mysql_article.go
+++ article/repository/mysql_article.go
@@ -14,7 +14,7 @@ import (
 )
 
 const (
-	timeFormat = "2006-01-02T15:04:05.999Z07:00" // reduce precision from RFC3339Nano as date format in mongo only capable until milliseconds
+	timeFormat = "2006-01-02T15:04:05.999Z07:00" // reduce precision from RFC3339Nano as date format
 )
 
 type mysqlArticleRepository struct {
