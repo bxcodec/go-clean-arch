f34ca0c589cbf3ef67bcb266559249b2e74f06f6
Iman Tumorang <iman.tumorang@gmail.com>
2020-04-18 12:47:05 +0700

    chore: add linter

--- article/delivery/http/middleware/middleware_test.go
+++ article/delivery/http/middleware/middleware_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/bxcodec/go-clean-arch/middleware"
+	"github.com/bxcodec/go-clean-arch/article/delivery/http/middleware"
 )
 
 func TestCORS(t *testing.T) {
--- main.go
+++ main.go
@@ -5,18 +5,17 @@ import (
 	"fmt"
 	"log"
 	"net/url"
-	"os"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo"
 	"github.com/spf13/viper"
 
-	_articleHttpDeliver "github.com/bxcodec/go-clean-arch/article/delivery/http"
+	_articleHttpDelivery "github.com/bxcodec/go-clean-arch/article/delivery/http"
+	_articleHttpDeliveryMiddleware "github.com/bxcodec/go-clean-arch/article/delivery/http/middleware"
 	_articleRepo "github.com/bxcodec/go-clean-arch/article/repository"
 	_articleUcase "github.com/bxcodec/go-clean-arch/article/usecase"
 	_authorRepo "github.com/bxcodec/go-clean-arch/author/repository"
-	"github.com/bxcodec/go-clean-arch/middleware"
 )
 
 func init() {
@@ -59,14 +58,14 @@ func main() {
 	}()
 
 	e := echo.New()
-	middL := middleware.InitMiddleware()
+	middL := _articleHttpDeliveryMiddleware.InitMiddleware()
 	e.Use(middL.CORS)
 	authorRepo := _authorRepo.NewMysqlAuthorRepository(dbConn)
 	ar := _articleRepo.NewMysqlArticleRepository(dbConn)
 
 	timeoutContext := time.Duration(viper.GetInt("context.timeout")) * time.Second
 	au := _articleUcase.NewArticleUsecase(ar, authorRepo, timeoutContext)
-	_articleHttpDeliver.NewArticleHandler(e, au)
+	_articleHttpDelivery.NewArticleHandler(e, au)
 
 	log.Fatal(e.Start(viper.GetString("server.address")))
 }
