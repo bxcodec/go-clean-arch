259575511c448a9147fb4945f85bd9c9d08fe0d7
Bogdan Novikov <bogdan.novikov@socialtechnologies.io>
2020-05-30 19:32:26 +0300

    fix errors style https://github.com/golang/go/wiki/CodeReviewComments#error-strings

--- domain/errors.go
+++ domain/errors.go
@@ -4,11 +4,11 @@ import "errors"
 
 var (
 	// ErrInternalServerError will throw if any the Internal Server Error happen
-	ErrInternalServerError = errors.New("Internal Server Error")
+	ErrInternalServerError = errors.New("internal Server Error")
 	// ErrNotFound will throw if the requested item is not exists
-	ErrNotFound = errors.New("Your requested Item is not found")
+	ErrNotFound = errors.New("your requested Item is not found")
 	// ErrConflict will throw if the current action already exists
-	ErrConflict = errors.New("Your Item already exist")
+	ErrConflict = errors.New("your Item already exist")
 	// ErrBadParamInput will throw if the given request-body or params is not valid
-	ErrBadParamInput = errors.New("Given Param is not valid")
+	ErrBadParamInput = errors.New("given Param is not valid")
 )
